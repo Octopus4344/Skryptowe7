aa95f0791a447c5f475c0fb5e0f426e8e520af36
Apolonia Abramowicz <pola.abramowicz@onet.pl>
2024-04-24 13:18:13 +0200

    drugie lepsze

--- zad_2.py
+++ zad_2.py
@@ -4,10 +4,18 @@ def forall(pred, iterable):
     return True
 
 
+def forall_all(pred, iterable):
+    return all(pred(item) for item in iterable)
+
+
 def exist(pred, iterable):
     for i in iterable:
         if pred(i): return True
     return False
+
+
+def exist_any(pred, iterable):
+    return any(pred(item) for item in iterable)
 
 
 def atleast(n, pred, iterable):
@@ -19,6 +27,11 @@ def atleast(n, pred, iterable):
     return False
 
 
+def atleast_sum(n, pred, iterable):
+    count = sum(1 for item in iterable if pred(item))
+    return count >= n
+
+
 def atmost(n, pred, iterable):
     k = 0;
     for i in iterable:
@@ -26,3 +39,7 @@ def atmost(n, pred, iterable):
             k += 1
             if k >= n: return False
     return False
+
+def atmost_sum(n, pred, iterable):
+    count = sum(1 for item in iterable if pred(item))
+    return count < n
